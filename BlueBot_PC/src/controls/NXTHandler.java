2dfe1f19714c2acd7f96ec5e5fb37217d20077c9
Incalza Dario <h4oxer@gmail.com>
2012-10-07 02:00:16 +0200

    Added Command enumerations, IHandler (interface), method skeleton for NXTHandler and SimulatorHandler.

---
+++ BlueBot_PC/src/controls/NXTHandler.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package controls;
+
+/**
+ * @author  Incalza Dario
+ *
+ */
+public class NXTHandler implements IHandler{
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
