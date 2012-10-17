ca076f731f7ec5a50890ae42670361005809e222
Lies <Lies@10.8.71.16>
2012-10-17 11:47:04 +0200

    dafault waardes aangepast

--- BlueBot_NXT/src/PilotController.java
+++ BlueBot_NXT/src/PilotController.java
@@ -4,14 +4,18 @@ import lejos.robotics.navigation.DifferentialPilot;
 
 public class PilotController {
 	
-	private DifferentialPilot pilot = new DifferentialPilot(53.2, 52.85,163, Motor.A, Motor.C, false);
+	private static DifferentialPilot pilot = new DifferentialPilot(55.37, 55,167.78, Motor.A, Motor.C, false);
+	private final static double defaultTravelSpeed = 300;
+	private final static int defaultAccelerationSpeed = 1000;
+	private final static double defaultRotationSpeed = 100;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		pilot.setAcceleration(defaultAccelerationSpeed);
+		pilot.setRotateSpeed(defaultRotationSpeed);
+		pilot.setTravelSpeed(defaultTravelSpeed);
 	}
 	
 	/**
