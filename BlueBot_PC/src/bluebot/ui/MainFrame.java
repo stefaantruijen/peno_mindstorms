882af6d300db929f6e113556209336f3752e4078
RubenBlueBot <nico.cooper-0m1z3ng@yopmail.com>
2012-10-30 20:19:52 +0100

    Code clean-up.

--- BlueBot_PC/src/bluebot/ui/MainFrame.java
+++ BlueBot_PC/src/bluebot/ui/MainFrame.java
@@ -9,7 +9,6 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
