bd7ad1ddcef15c5becd252fef1214d7ca164a1a1
Lies <Lies@10.43.11.88>
2013-04-23 18:05:09 +0200

    speed seesaw

--- BlueBot_Shared/src/bluebot/actions/impl/SeesawAction.java
+++ BlueBot_Shared/src/bluebot/actions/impl/SeesawAction.java
@@ -19,14 +19,15 @@ public class SeesawAction extends Action{
 	
 	private void seesaw() throws ActionException, DriverException, InterruptedException{
 		int speed = this.getDriver().getSpeed();
-		this.getDriver().setSpeed(20);
+		this.getDriver().setSpeed(50);
 		this.getDriver().moveForward();
 		while(this.getDriver().isMoving() && !this.getDriver().readsGray());
 		while(this.getDriver().isMoving() && !this.getDriver().readsBlack());
+		this.getDriver().setSpeed(12);
 		while(this.getDriver().isMoving() && !this.getDriver().readsGray());
 		this.getDriver().stop();
 		white.execute(this.getDriver());
-		this.getDriver().moveBackward(20, true);
+		this.getDriver().moveForward(20, true);
 		this.getDriver().setSpeed(speed);
 	}
 
