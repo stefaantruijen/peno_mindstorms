172b7cc60df498cec8444afb0c9fd658e4116c47
Ine <Ine@10.43.19.194>
2013-05-01 12:40:42 +0200

    Stop toegevoegd

--- BlueBot_Shared/src/bluebot/actions/impl/SeesawAction.java
+++ BlueBot_Shared/src/bluebot/actions/impl/SeesawAction.java
@@ -19,16 +19,27 @@ public class SeesawAction extends Action{
 	
 	private void seesaw() throws ActionException, DriverException, InterruptedException{
 		int speed = this.getDriver().getSpeed();
+		System.out.println("STARTED SEESAW");
 		this.getDriver().setSpeed(50);
+		System.out.println("STARTED SEESAW 1");
 		this.getDriver().moveForward();
+		System.out.println("STARTED SEESAW 2");
 		while(this.getDriver().isMoving() && !this.getDriver().readsGray());
+		System.out.println("STARTED SEESAW 3");
 		while(this.getDriver().isMoving() && !this.getDriver().readsBlack());
+		System.out.println("STARTED SEESAW 4");
 		this.getDriver().setSpeed(12);
+		System.out.println("STARTED SEESAW 5");
 		while(this.getDriver().isMoving() && !this.getDriver().readsGray());
+		System.out.println("STARTED SEESAW 6");
 		this.getDriver().stop();
+		System.out.println("STARTED SEESAW 7");
 		white.execute(this.getDriver());
+		System.out.println("STARTED SEESAW 8");
 		this.getDriver().moveForward(20, true);
+		System.out.println("STARTED SEESAW 9");
 		this.getDriver().setSpeed(speed);
+		System.out.println("ENDED SEESAW");
 	}
 
 }
